6b670f6e678f1ee04b83f566c7cde0b29ac6c684
Paras-Tiwari-18 <151220289+Paras-Tiwari-18@users.noreply.github.com>
2024-06-28 00:43:47 +0530

    Add files via upload

---
+++ brief1.js
@@ -0,0 +1,21 @@
+//ye wala main div me hoga
+const brief1_data = [
+    {
+        heading:"Civil Engineering Department",
+        description:"The KIET Group of Institutions offers Total Station land measurement, concrete mix design and cube testing, and Non-Destructive Testing (NDT) for concrete using tools like rebound hammers, UPV testers, and rebar locators to assess structural integrity and corrosion."    
+    },
+    {
+        heading:"Mechanical Engineering Department",
+        description:"The KIET Group of Institutions' Mechanical Engineering Department offers comprehensive material testing services, including surface roughness, impact, NDT, ultrasonic flaw detection, computerized UTM, fatigue, torsion, and high-temperature testing. Their machining and casting services feature stir casting, lathe/milling/drilling, vertical milling, and plasma cutting. They also provide internal combustion engine testing, including Morse tests, VCR petrol engines, diesel engines, exhaust gas analysis, and smoke measurement."
+    },
+    {
+        heading:"Electrical and Electronics Engineering Department",
+        description:"The KIET Group of Institutions' Electrical and Electronics Engineering Department offers advanced simulation and prototyping services with a prototype PCB machine, OPAL-RT for real-time simulation, and various digital storage oscilloscopes. They also conduct power quality analysis and energy auditing using single-phase and power quality analyzers. Additionally, they provide electrical design and feasibility analysis for PV-based solar power systems using PVsyst 6 PRO 30 with industrial datasheets of PV panels and inverters."
+    },
+    {
+        heading:"KSOP Department",
+        description:"The KSOP Department at KIET Group of Institutions features a Formulation Development Lab equipped with a rotary tablet compression machine, tablet coating pan, spheronizer, and various equipment for coating, pelletization, and granulation. It also includes quality control instruments like a dissolution test apparatus, hardness testers, and a friability test apparatus. Additionally, they have advanced tools for nanosuspension formulation and solvent evaporation. The Analytical Method Development Lab offers FT-IR for qualitative and quantitative analysis and HPLC for high-pressure liquid chromatography with variable wavelength detection."
+    },
+]
+
+export default brief1_data;
